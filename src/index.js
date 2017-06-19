615903338565e7eb3f64a33e23a31c4d95f1abad
Jatin Nayak <jatin.nayak@happiestminds.com>
2017-06-19 16:15:37 +0530

    es6 feature

--- src/index.js
+++ src/index.js
@@ -15,5 +15,90 @@ class EcmaSix {
   constructor() {
     this.a = 10;
     this.b = 20;
+    this.arr = [1, 2, 3, 4, 5];
+  }
+
+  arrayParse() {
+    arr.forEach((e) => {
+      return e * e;
+    });
+  }
+
+  stringliterals() {
+    let customer = { name: "Foo" },
+        card = { amount: 7, product: "Bar", unitprice: 42 },
+        message = `Hello ${customer.name}`;
   }
 }
+
+let esSix = new EcmaSix();
+esSix.arrayParse();
+
+
+//Inheritance
+
+class Parent {
+  constructor() {
+    console.log('In Parent Class');
+  }
+}
+
+class Child extends Parent{
+  constructor() {
+    // Here, it calls the parent class' constructor
+    super();
+    // Note: In derived classes, super() must be called before you
+    // can use 'this'. Leaving this out will cause a reference error
+    console.log('In Child Class');
+    
+  }
+}
+
+//Static Method
+class StaticMethodCall {
+  static staticMethod() {
+    return 'Static method has been called';
+  }
+  static anotherStaticMethod() {
+    return this.staticMethod() + ' from another static method';
+  }
+}
+StaticMethodCall.staticMethod(); 
+// 'Static method has been called'
+
+StaticMethodCall.anotherStaticMethod(); 
+// 'Static method has been called from another static method'
+
+
+
+//Map
+let map = new Map().set(1, 'one')
+                   .set(2, 'two')
+                   .set(3, 'three');
+map.get(1);
+
+
+//Promise
+var aPromise = () => {
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+			console.log('Promise Called');
+			resolve('Success');
+        }, 300);
+	});
+}
+aPromise().then(aPromise())
+        .then(aPromise())
+        .then(aPromise())
+        .then(aPromise())
+        .then((data) => {
+            console.log(data);
+        });
+
+Promise.all([
+  aPromise(),
+	aPromise(),
+	aPromise()
+]).then((data) => {
+    console.log(data);
+});     
